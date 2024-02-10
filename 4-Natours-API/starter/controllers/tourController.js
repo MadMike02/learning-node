ccc3ee57a2dd874d0bca53f58fdb452bc9103b3a
sahil jassal <sahiljassal9198@gmail.com>
2024-02-10 13:33:20 +0530

    seprate logic to seprate file

--- 4-Natours-API/starter/controllers/tourController.js
+++ 4-Natours-API/starter/controllers/tourController.js
@@ -1,6 +1,6 @@
 // const fs = require('fs');
 const Tour = require('../models/tourModel');
-
+const APIFeatures = require('../utils/ApiFeatures');
 // const tours = JSON.parse(
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 // );
@@ -15,51 +15,14 @@ exports.aliasTopTour = async (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    const queryObj = { ...req.query };
-    const exculdeFields = ['page', 'sort', 'limit', 'fields'];
-    exculdeFields.forEach((el) => delete queryObj[el]);
-    //adding $ sign for gte and other filters
-    //getting ---- { duration: { gte: '5' }, difficulty: 'easy' }
-    //required ---  { duration: { $gte: '5' }, difficulty: 'easy' }
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    let query = Tour.find(JSON.parse(queryStr));
+    console.log(req.query);
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
 
-    //sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      console.log(req.query.sort, sortBy);
-      query = query.sort(sortBy);
-    } else {
-      query.sort('-createdAt');
-    }
-
-    //fields limiting
-    if (req.query.fields) {
-      const field = req.query.fields.split(',').join(' ');
-      query = query.select(field);
-    } else {
-      query.select('-__v');
-    }
-
-    //pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('Inavalid page number');
-    }
-
-    // const tours = await Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
-    const tours = await query;
+    const tours = await features.query;
     res.status(200).json({
       status: 'success',
       results: tours.length,
