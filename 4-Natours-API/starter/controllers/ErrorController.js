fa1573d8b639f35ac66e6a45cd730823b7ae2cea
sahil jassal <sahiljassal9198@gmail.com>
2024-02-12 17:03:38 +0530

    error handling for unidentified ids from mongoose

--- 4-Natours-API/starter/controllers/ErrorController.js
+++ 4-Natours-API/starter/controllers/ErrorController.js
@@ -1,3 +1,5 @@
+const AppError = require('../utils/AppErros');
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -23,13 +25,20 @@ const sendErrorProd = (err, res) => {
   }
 };
 
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path} : ${err.value}`;
+  return new AppError(message, 400);
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
-  } else if (process.env.NODE_ENV === 'development') {
-    sendErrorProd(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    if (err.name === 'CastError') 
+      error = handleCastErrorDB(error);
+    sendErrorProd(error, res);
   }
 };
